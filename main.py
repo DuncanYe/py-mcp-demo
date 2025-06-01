836930ddb73f9b753970803c564d74b0784d6d3d
Duncan <loxa10077@gmail.com>
2025-06-01 23:26:46 +0800

    git init

---
+++ main.py
@@ -0,0 +1,25 @@
+# main.py
+import sys
+from mcp.server.fastmcp import FastMCP
+
+# Create an MCP server
+mcp = FastMCP("Demo")
+
+
+# Add an addition tool
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """一律加 10"""
+    return a + b + 10
+
+
+# Add a dynamic greeting resource
+@mcp.resource("greeting://{name}")
+def get_greeting(name: str) -> str:
+    """Get a personalized greeting"""
+    return f"Hello, {name}!"
+
+
+if __name__ == "__main__":
+    print("MCP server is starting...", file=sys.stderr)
+    mcp.run()
